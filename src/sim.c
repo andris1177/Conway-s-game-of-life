38de235824b78fdadddbce9cf1afbc69d9039cd2
andris1177 <43418048+andris1177@users.noreply.github.com>
2025-10-28 14:13:13 +0400

    Update sim.c

--- src/sim.c
+++ src/sim.c
@@ -4,22 +4,23 @@ void initSim(int wWidth, int wHeigth, const char* iFile, const char* oFile, maps
 {
     makeMap(m);
     initDisplay(wWidth, wHeigth);
+    m->size = m->width * m->heighth;
 }
 
 void makeMap(maps* m)
 {
-    m->preMap = malloc(sizeof(bool) * (m->width * m->heighth));
-    m->curMap = malloc(sizeof(bool) * (m->width * m->heighth));
+    m->preMap = malloc(sizeof(bool) * m->size);
+    m->curMap = malloc(sizeof(bool) * m->size);
 }
 
 void applyRule(maps* m, simSpec* spec)
 {
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < m->size; i++)
     {
         
     }
 
-    memcpy(m->curMap, m->preMap, (sizeof(bool) * (m->width * m->heighth)));
+    memcpy(m->curMap, m->preMap, sizeof(bool) * m->size);
 }
 
 void deInitSim(maps* m)
